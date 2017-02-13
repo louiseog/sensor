317bc2ef933857afba49a4976a3c97a3bdfabe23
Louise O'Gorman <logorman@e1013-09.campus.itsligo.ie>
2017-02-13 15:25:43 +0000

    sensor file

---
+++ SendDataToGoogleSpreadsheet/TembooAccount.h
@@ -0,0 +1,5 @@
+#define TEMBOO_ACCOUNT "louiseog"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app key name
+#define TEMBOO_APP_KEY  "4aXHBeqmIWX08DnrVttCFpJ07JL5YTuN"  // your Temboo app key
+
+
